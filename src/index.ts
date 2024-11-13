74164a040ccbfee90595bd0cc9e95ef680091207
NicolasVelasco <nicolas.velasco@alumnos.frm.utn.edu.ar>
2024-11-12 21:47:32 -0300

    NODE_ENV

--- src/index.ts
+++ src/index.ts
@@ -30,29 +30,30 @@ apiV1Router.use("/session", Routes.sessionRouter());
 
 app.use("/docs", swaggerUi.serve);
 
-//Utilizar este en desarrollo
-//app.get("/docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+const checkMode = process.env.NODE_ENV;
+console.log(checkMode);
 
-//Utilizar este en produccion
+if (checkMode === "development") {
+  //Utilizar este en desarrollo
+  app.get("/docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+} else if (checkMode === "production") {
+  //Utilizar este en produccion
+  const swaggerConfig: SwaggerUiOptions = {
+    customSiteTitle: 'Backend Generator',
+    customfavIcon: 'https://avatars.githubusercontent.com/u/185267919?s=400&u=7d74f9c123b27391d3f11da2815de1e9a1031ca9&v=4',
+    customJs: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js',
+    ],
+    customCssUrl: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.css',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css',
+    ],
+  };
 
-
-const swaggerConfig: SwaggerUiOptions = {
-  customSiteTitle: 'Backend Generator',
-  customfavIcon: 'https://avatars.githubusercontent.com/u/185267919?s=400&u=7d74f9c123b27391d3f11da2815de1e9a1031ca9&v=4',
-  customJs: [
-    'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js',
-    'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js',
-  ],
-  customCssUrl: [
-    'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
-    'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.css',
-    'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css',
-  ],
-};
-
-app.get("/docs", swaggerUi.setup(swaggerFile, swaggerConfig));
-
-
+  app.get("/docs", swaggerUi.setup(swaggerFile, swaggerConfig));
+}
 
 // Middleware para manejar rutas no encontradas
 app.use((_req: Request, res: Response) => {
