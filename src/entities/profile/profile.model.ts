e92ac543b6b4120e36e0e69588366d9ca6f58c99
NicolasVelasco <nicolas.velasco@alumnos.frm.utn.edu.ar>
2024-11-12 23:32:25 -0300

    moddel

--- src/entities/profile/profile.model.ts
+++ src/entities/profile/profile.model.ts
@@ -1,9 +1,10 @@
-import { Entity, ManyToOne } from "typeorm";
+import { Column, Entity, ManyToOne } from "typeorm";
 import { Base } from "../base/base.model";
 import { Enterprise } from "../enterprise";
 
 @Entity("profiles")
 export class Profile extends Base {
+  @Column()
   @ManyToOne(() => Enterprise, (enterprise) => enterprise.profiles, {
     onDelete: "CASCADE",
     onUpdate: "CASCADE",
