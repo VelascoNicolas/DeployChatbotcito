c1e844b56f1a3ca7035fde9d87ee9b77214e9118
NicolasVelasco <nicolas.velasco@alumnos.frm.utn.edu.ar>
2024-11-14 16:38:11 -0300

    new swagger

--- src/index.ts
+++ src/index.ts
@@ -45,7 +45,7 @@ if(modo === "production") {
       'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js',
       'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js',
     ],
-    customCss: [
+    customCssUrl: [
       'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
       'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.css',
       'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css',
